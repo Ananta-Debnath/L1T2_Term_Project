d724e428a021dcefca94d3db16f7742f0d1d7e98
Ananta Debnath <ananta.deb2005@gmail.com>
2025-06-26 01:20:03 +0600

    Setup javafx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.l1t2_term_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.l1t2_term_project to javafx.fxml;
+    exports com.example.l1t2_term_project;
+}
