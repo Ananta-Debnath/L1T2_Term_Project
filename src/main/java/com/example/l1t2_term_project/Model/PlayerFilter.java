ae49dbe8368b923d3c70e75eaf3b9c38def0c128
Ananta Debnath <ananta.deb2005@gmail.com>
2025-07-04 20:07:24 +0600

    Add PlayerFilter class

---
+++ src/main/java/com/example/l1t2_term_project/Model/PlayerFilter.java
@@ -0,0 +1,50 @@
+package com.example.l1t2_term_project.Model;
+
+public class PlayerFilter
+{
+    private String name;
+    private String position;
+    private String nationality;
+    private String team;
+    private boolean forSale;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public boolean isForSale() {
+        return forSale;
+    }
+
+    public void setForSale(boolean forSale) {
+        this.forSale = forSale;
+    }
+}
