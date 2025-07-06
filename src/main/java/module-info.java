342a521f54b24a103509435f3551d483493efb18
Ananta Debnath <ananta.deb2005@gmail.com>
2025-07-06 18:38:39 +0600

    Add Market.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,6 @@ module com.example.l1t2_term_project {
     requires net.synedra.validatorfx;
     requires com.almasb.fxgl.all;
 
-    opens com.example.l1t2_term_project to javafx.fxml;
+    opens com.example.l1t2_term_project.Controller to javafx.fxml;
     exports com.example.l1t2_term_project;
 }
