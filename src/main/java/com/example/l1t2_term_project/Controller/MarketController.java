f6a26a6f8f9d99e45beb72b0148e67d6fe68f19d
Ananta Debnath <ananta.deb2005@gmail.com>
2025-07-08 20:09:02 +0600

    Update MarketController - filter, cancel, reset, save button functionality

--- src/main/java/com/example/l1t2_term_project/Controller/MarketController.java
+++ src/main/java/com/example/l1t2_term_project/Controller/MarketController.java
@@ -1,55 +1,137 @@
 package com.example.l1t2_term_project.Controller;
 
 import com.example.l1t2_term_project.Model.Player;
+import com.example.l1t2_term_project.Model.PlayerFilter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
+
+/*
+TODO list
+TODO: make role field have appropriate items according to position field
+TODO: Use enum in appropriate places
+ */
+
+
 public class MarketController
 {
-    @FXML
-    private VBox filterBox;
+    // Non-FXML variables
+    PlayerFilter filter = new PlayerFilter();
 
     @FXML
-    ListView<Player> itemList;
+    public AnchorPane mainMenu;
+    @FXML
+    public TextField searchField;
 
     @FXML
-    public Button cancelButton;
+    public VBox filterBox;
+    @FXML
+    public ComboBox<String> positionField; // TODO: Replace with enum
+    @FXML
+    public ComboBox<String> roleField; // TODO: Replace with enum
+    @FXML
+    public ComboBox<String> nationField;
+    @FXML
+    public ComboBox<String> clubField;
+    @FXML
+    public TextField minValueField;
+    @FXML
+    public TextField maxValueField;
+    @FXML
+    public CheckBox availabilityField;
+
 
     @FXML
-    private AnchorPane mainMenu;
-
-    @FXML
-    private TextField searchField;
-
-    @FXML
-    private Button filterButton;
-
-    @FXML
-    public void onFilterClick()
+    private void initialize()
     {
-        // mainMenu.setVisible(false);
+        // TODO: Set the items in the ComboBox
+        // Need player collection class for this
+        // NOTE: the first element has to indicate null value
+    }
+
+    @FXML
+    public void openFilter() // Filter Button
+    {
         mainMenu.setDisable(true);
         filterBox.setVisible(true);
-        System.out.println("Filter");
-//        itemList.getItems().clear();
-//        itemList.getItems().add(new Player());
+        // System.out.println("Filter");
     }
 
     @FXML
-    public void onCancelClick()
+    public void searchPlayers()
     {
-        mainMenu.setDisable(false);
-        mainMenu.setVisible(true);
-        filterBox.setVisible(false);
-        System.out.println("Cancel");
+        // TODO: get Players from Player Collection
+        filter.setName(searchField.getText());
+        System.out.println(filter);
     }
 
     @FXML
-    public void onSearchClick(ActionEvent actionEvent) {
+    public void setFilter() // Save Button
+    {
+        filter = getFilterFromFields();
+        filterBox.setVisible(false);
+        mainMenu.setDisable(false);
+        searchPlayers();
+    }
+
+    @FXML
+    public void resetFilter()
+    {
+        setFieldsInFilterBox(new PlayerFilter());
+    }
+
+    @FXML
+    public void cancelFilter()
+    {
+        filterBox.setVisible(false);
+        mainMenu.setDisable(false);
+        setFieldsInFilterBox(filter); // Rollback to initial value
+        // System.out.println("Cancel");
+    }
+
+    // Non-FXML methods
+    private PlayerFilter getFilterFromFields()
+    {
+        PlayerFilter filter = new PlayerFilter();
+
+        filter.setPosition(positionField.getValue());
+        filter.setSpecificPosition(roleField.getValue()); // TODO: Change to role
+        filter.setNationality(nationField.getValue());
+        filter.setTeam(clubField.getValue());
+
+        if (!minValueField.getText().isEmpty()) filter.setStartingValue(Integer.parseInt(minValueField.getText()));
+        else filter.setStartingValue(0);
+
+        if (!maxValueField.getText().isEmpty()) filter.setEndingValue(Integer.parseInt(maxValueField.getText()));
+        else filter.setEndingValue(0);
+
+        filter.setForSale(availabilityField.isSelected());
+        return filter;
+    }
+
+    private void setFieldsInFilterBox(PlayerFilter filter)
+    {
+        if (filter.getPosition() != null) positionField.setValue(filter.getPosition()); // TODO: Replace with enum
+        else positionField.setValue(positionField.getItems().get(0));
+
+        if (filter.getSpecificPosition() != null) roleField.setValue(filter.getSpecificPosition()); // TODO: Change get method to role & Replace with enum
+        else roleField.setValue(roleField.getItems().get(0));
+
+        if (filter.getNationality() != null) nationField.setValue(filter.getNationality());
+        else nationField.setValue(nationField.getItems().get(0));
+
+        if (filter.getSpecificPosition() != null) clubField.setValue(filter.getTeam());
+        else roleField.setValue(roleField.getItems().get(0));
+
+        if (filter.getStartingValue() != 0) minValueField.setText(Integer.toString(filter.getStartingValue()));
+        else minValueField.setText(null);
+
+        if (filter.getEndingValue() != 0) maxValueField.setText(Integer.toString(filter.getEndingValue()));
+        else maxValueField.setText(null);
+
+        availabilityField.setSelected(filter.isForSale());
     }
 }
