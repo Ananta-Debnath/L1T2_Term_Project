eff235c77d3bee39bdd6ba04ff467743c9753da8
Ananta Debnath <ananta.deb2005@gmail.com>
2025-07-28 03:05:00 +0600

    Add change password feature

--- src/main/java/com/example/l1t2_term_project/Controller/ClubDetailsController.java
+++ src/main/java/com/example/l1t2_term_project/Controller/ClubDetailsController.java
@@ -1,13 +1,20 @@
 package com.example.l1t2_term_project.Controller;
 
+import com.example.l1t2_term_project.Client;
+import com.example.l1t2_term_project.DTO.LoginDTO;
 import com.example.l1t2_term_project.Model.Club.Club;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.io.IOException;
 
 public class ClubDetailsController {
 
@@ -32,11 +39,13 @@ public class ClubDetailsController {
     @FXML
     public PasswordField currentPasswordField;
 
+    private Client client;
     private Club currentClub;
 
-    public void setClub(Club c){
-            this.currentClub=c;
-            updateClubDetails();
+    public void initializeValues(Client client, Club c){
+        this.client = client;
+        this.currentClub=c;
+        updateClubDetails();
     }
 
     private void updateClubDetails(){
@@ -68,5 +77,56 @@ public class ClubDetailsController {
 
 
     public void handlePasswordChange(ActionEvent actionEvent) {
+        if (!newPasswordField.getText().equals(confirmPasswordField.getText()))
+        {
+            showAlert("Password mismatch", "New Password and Confirm Password does not match");
+            return;
+        }
+
+        LoginDTO loginDTO = new LoginDTO(currentClub.getName(), currentPasswordField.getText(), LoginDTO.Type.SignIn);
+        client.write(loginDTO);
+        Object obj = client.read();
+        if (obj instanceof Boolean)
+        {
+            boolean valid = (boolean) obj;
+            if (!valid)
+            {
+                showAlert("Wrong Password", "The provided current password is wrong");
+                return;
+            }
+        }
+        else {
+            System.err.println("Wrong object type - " + obj.getClass());
+            return;
+        }
+
+        loginDTO = new LoginDTO(currentClub.getName(), newPasswordField.getText(), LoginDTO.Type.ChangePass);
+        client.write(loginDTO);
+        obj = client.read();
+        if (obj instanceof Boolean)
+        {
+            boolean valid = (boolean) obj;
+            if (valid)
+            {
+                showAlert("Password Changed", "New password set successfully");
+            }
+            else
+            {
+                showAlert("Failed!", "User not found");
+            }
+        }
+        else {
+            System.err.println("Wrong object type - " + obj.getClass());
+        }
     }
+
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
 }
