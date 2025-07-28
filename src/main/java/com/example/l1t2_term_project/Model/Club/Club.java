95ef770f54f62d9481528e0527caa7d02d108f58
Wasik Samin <wasiksamin24@gmail.com>
2025-07-28 21:21:25 +0600

    Merge pull request #15
    * updated playerlist controller
    
    * updated market.fxml
    
    * fixed hieght width

--- src/main/java/com/example/l1t2_term_project/Model/Club/Club.java
+++ src/main/java/com/example/l1t2_term_project/Model/Club/Club.java
@@ -140,24 +140,6 @@ public class Club implements Serializable{
     }
 
 
-
-
-
-    public String toCSVLine(){
-
-        StringBuilder str=new StringBuilder();
-
-        str.append(name).append(",");
-        str.append(leagueName).append(",");
-        str.append(country).append(",");
-        str.append(budget).append(",");
-        str.append(stadiumName).append(",");
-        str.append(managerName);
-
-        return str.toString();
-
-    }
-
     public static Club readFromServer(Client client)
     {
         client.write(client.getCurrentClub());
@@ -201,6 +183,23 @@ public class Club implements Serializable{
     }
 
 
+
+    public String toCSVLine(){
+
+        StringBuilder str=new StringBuilder();
+
+        str.append(name).append(",");
+        str.append(leagueName).append(",");
+        str.append(country).append(",");
+        str.append(budget).append(",");
+        str.append(stadiumName).append(",");
+        str.append(managerName);
+
+        return str.toString();
+
+    }
+
+    
     /*
     public class ClubManager {
     private List<Club> clubs;
